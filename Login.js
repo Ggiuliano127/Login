98f1f6aad2555372c67570d6c01af0a989a4e1db
Ggiuliano127 <137308320+Ggiuliano127@users.noreply.github.com>
2023-09-20 11:18:53 -0300

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,14 @@
+username=="kurdt";
+password=="4446661";
+
+function autenticar(){
+    var lgn = document.getElementById('Login');
+    var pwd = document.getElementById('passwd');
+
+    if(lgn == username & pwd == password){
+        alert('fon')
+    }else{
+    alert('firifonfon')
+    }
+
+}
